2fa88f444ecf8358139fdd141403758937492436
Brian Truman <34318282+bttruman@users.noreply.github.com>
2020-12-14 13:47:03 -0600

    Dummy Commit

--- nibrslists.js
+++ nibrslists.js
@@ -5,7 +5,7 @@ var nibrs = {
     property: "<ul><li><b>120</li><li>200, 210, 220, 240, 250, 270, 280, 290</li><li>26A, 26B, 26C, 26D, 26E</li><li>23A, 23B, 23C, 23D, 23E, 23F, 23G, 23H</li><li>510</li><li>90A, 90Z</b> (If Applicable)</li></ul>",
     noproperty: "<ul><li><b>09A, 09B, 09C</li><li>11A, 11B, 11C, 11D</li><li>13A, 13B, 13C</li><li>36A, 36B</li><li>370</li><li>40A, 40B</li><li>520</li><li>90B through 90Z</b> (If Applicable)</li></ul>"
 };
-
+ 
 var weapons = {
     aggAssault: "<ul><li>11/11A Firearm or Automatic Firearm</li><li>12/12A Handgun or Automatic Handgun</li><li>13/13A Rifle or Automatic Rifle</li><li>14/14A Shotgun or Automatic Shotgun</li><li>15/15A Other Firearm or Other Automatic Firearm</li></ul>",
     simAssault: "<ul><li>Personal Weapons (40)</li><li>Other (90)</li><li>Unknown (95)</li></ul>",
